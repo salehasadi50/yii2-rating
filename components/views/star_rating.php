f108529cb0137aeb3c6ca476d3d4f96d477e0af7
Saleh Asadi <sa.asadi@keylid.com>
2016-08-10 15:51:18 +0430

    update akhar
    update akhar

--- components/views/star_rating.php
+++ components/views/star_rating.php
@@ -3,7 +3,7 @@
 <?= kartik\rating\StarRating::widget(['name' => 'rating_' . $service_id . '_' . $item_id,'pluginOptions' =>['showClear'=>false], 'pluginEvents' =>["rating.change" => "send_favorite_" . $service_id . "_". $item_id]]);?>
 <script type="text/javascript">
 	var send_favorite_<?=$service_id ?>_<?= $item_id ?> = function(event, value, caption) {
-		 // $('.rate') .click (function(){
+		  $('.rate') .click (function(){
 
 		    	$.ajax({
 		             url : '<?= Yii::getAlias('@web')?>/rating/default/ajax',
@@ -16,7 +16,7 @@
 		             success:function(data){
 		             	console.log(data);
 		             }
-		          // });
+		           });
 	         });
 	}
 	</script>
